40932ec8bd511e2e77b31c68a2b2bb4a02cd3188
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-10-27 17:25:46 -0700

    Added some starter code for the server.js file

--- app/server.js
+++ app/server.js
@@ -1,1 +1,17 @@
 // Server file
+import express from 'express';
+import { initDb } from './database/db.js';
+import router from './router/router.js';
+
+const app = express();
+app.use(express.json);
+
+await initDb();
+
+app.use('/', router);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
